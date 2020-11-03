ac7a948847be0fbfe3cc74f9d6d5dda9b331f9bb
Sanoj Vaidyan <sanoj.vaidyan@rackspace.com>
2020-11-03 14:25:01 +0530

    Deletion action and all

--- address_book/views.py
+++ address_book/views.py
@@ -20,3 +20,28 @@ def add_address(request):
 			return render(request, "add_address.html")
 	else:
 		return render(request, "add_address.html", {})
+
+def edit(request, list_id):
+	if request.method == "POST":
+		record = Address.objects.get(pk=list_id)
+		form = AddressForm(request.POST or None, instance=record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ("Address Contact has been Editted!!"))
+			return redirect("home")
+		else:
+			messages.success(request, ("Some issue with the Data updated!!"))
+			return render(request, "edit.html", {"get_address" : record})
+	else:
+		record = Address.objects.get(pk=list_id)
+		return render(request, "edit.html", {"get_address" : record})
+
+def delete(request, list_id):
+	if request.method == "POST":
+		record = Address.objects.get(pk=list_id)
+		record.delete()
+		messages.success(request, ("Address Contact has been Deleted!!"))
+		return redirect("home")
+	else:
+		messages.success(request, ("Some issue with Deletion!!"))
+		return redirect("home")
